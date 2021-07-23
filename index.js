1bdcd1f64e6349f01fa8eabd20e9d4e462ea0a6e
mAsadSiddique <asadsiddiuque.a@gmail.com>
2021-07-23 11:20:22 +0500

    Created index.html and index.js file

---
+++ index.js
@@ -0,0 +1,35 @@
+/* REPEL
+        1)  JS EXPRESSION
+        2)  USE VARIABLES
+        3)  MULTILINE CODE
+        4)  USE (_) TO GET THE RESULT
+        5)  WE CAN USE EDITOR MODE  
+*/
+
+//  1)  JS EXPRESSION
+
+2 + 2
+
+
+//    2)  USE VARIABLES
+
+const num = 2;
+
+
+//   3)  MULTILINE CODE
+
+
+for(i=0; i < 5; i++)
+{
+    console.log(i)
+}
+
+//    4)  USE (_) TO GET THE RESULT
+
+5 + 5
+
+15 + 5
+
+_ * 2
+
+// 5)  WE CAN USE EDITOR MODE 
